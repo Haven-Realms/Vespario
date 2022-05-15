000d89e1eaf90bb001c408e7af7a9fafd5b3bae2
Dan Alexander <tereptis@gmail.com>
2022-05-15 13:30:08 +1200

    Debug

--- Vespario.py
+++ Vespario.py
@@ -22,6 +22,7 @@ class Vespario(commands.Bot):
         # Vespario Base Properties
         self.prefixes = {}
         self.token = str(sys.argv[0])
+        print(self.token)
         self.enabled = False
         self.id = "822313634474229760"
         
